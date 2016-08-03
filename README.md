61f582d24d921b91bbc96846c0c4839486075272
Joe Fitzgerald <jfitzgerald@pivotal.io>
2016-08-02 18:20:52 -0700

    Find Anti-Affinity Rules

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 
 Dependencies are vendored with [Glide](https://github.com/Masterminds/glide)
+
+## Environment Variables
+
+```
+export VSPHERE_SCHEME="https"
+export VSPHERE_HOSTNAME="localhost"
+export VSPHERE_PORT="1234"
+export VSPHERE_USERNAME="administrator"
+export VSPHERE_PASSWORD="password"
+export VSPHERE_INSECURE="true" # Ignore SSL Cert Errors
+```
--- main.go
+++ main.go
@@ -2,20 +2,106 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/url"
+	"os"
 
+	"golang.org/x/net/context"
+
+	"github.com/kelseyhightower/envconfig"
 	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/find"
+	"github.com/vmware/govmomi/property"
+	"github.com/vmware/govmomi/vim25/mo"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
-func main() {
-	ctx := nil //http://godoc.org/golang.org/x/net/context#Context
-	url := nil //http://godoc.org/net/url#Userinfo
-	insecure := false
+type vsphereconfig struct {
+	Scheme   string `default:"https"`
+	Hostname string `required:"true"`
+	Port     string `default:"443"`
+	Username string `required:"true"`
+	Password string `required:"true"`
+	Insecure bool   `default:"false"`
+}
 
-	client, err := govmomi.NewClient(ctx, url, insecure)
+func (c *vsphereconfig) HostAndPort() string {
+	if c.Scheme == "http" && c.Port != "80" {
+		return fmt.Sprintf("%s:%s", c.Hostname, c.Port)
+	}
+	if c.Scheme == "https" && c.Port != "443" {
+		return fmt.Sprintf("%s:%s", c.Hostname, c.Port)
+	}
+	return c.Hostname
+}
+
+func main() {
+	var config vsphereconfig
+	err := envconfig.Process("vsphere", &config)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	urlString := fmt.Sprintf("%s://%s:%s@%s/sdk", config.Scheme, url.QueryEscape(config.Username), url.QueryEscape(config.Password), config.HostAndPort())
+
+	url, err := url.Parse(urlString)
+	if err != nil {
+		panic(fmt.Sprintf("Error: %s\n", err))
+	}
+
+	c, err := govmomi.NewClient(ctx, url, config.Insecure)
 
 	if err != nil {
 		panic(fmt.Sprintf("Failed to initialize vmware.govmomi Client: %v", err))
 	}
 
-	fmt.Printf("Connected to a vCenter? %v", client.IsVC())
+	fmt.Println(fmt.Sprintf("Connected to a vCenter? %v", c.IsVC()))
+
+	f := find.NewFinder(c.Client, true)
+
+	// Find one and only datacenter
+	dc, err := f.DefaultDatacenter(ctx)
+	if err != nil {
+		exit(err)
+	}
+
+	// Make future calls local to this datacenter
+	f.SetDatacenter(dc)
+	clusters, err := f.ClusterComputeResourceList(ctx, "*")
+	fmt.Println("Fetching Clusters...")
+	for _, cluster := range clusters {
+		fmt.Println(fmt.Sprintf("Cluster: %s", cluster.Name()))
+		hosts, _ := cluster.Hosts(ctx)
+		for _, host := range hosts {
+			fmt.Println(fmt.Sprintf("Host: %s", host.Name()))
+		}
+	}
+
+	pc := property.DefaultCollector(c.Client)
+
+	// Convert clusters into list of references
+	var refs []types.ManagedObjectReference
+	for _, cluster := range clusters {
+		refs = append(refs, cluster.Reference())
+	}
+
+	// Retrieve summary property for all clusters
+	var mclusters []mo.ClusterComputeResource
+	err = pc.Retrieve(ctx, refs, nil, &mclusters)
+	if err != nil {
+		exit(err)
+	}
+	for _, cluster := range mclusters {
+		fmt.Println(cluster.Name)
+		for _, rule := range cluster.Configuration.Rule {
+			ruleinfo := rule.GetClusterRuleInfo()
+			fmt.Println(fmt.Sprintf("Rule: %s", ruleinfo.Name))
+		}
+	}
+}
+
+func exit(err error) {
+	fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+	os.Exit(1)
 }
