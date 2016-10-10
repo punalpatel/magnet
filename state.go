32ea4f3c7e9fd295e14f0f66bc8faa42d681596c
Zac Bergquist <zbergquist99@gmail.com>
2016-10-10 11:08:51 -0600

    [#131741833] initial state logic
    First pass at determining whether the state of the deployment is balanced or not.

--- state.go
+++ state.go
@@ -3,6 +3,8 @@ package magnet
 import (
 	"context"
 	"io"
+	"log"
+	"math"
 )
 
 func Check(ctx context.Context, i IaaS) error {
@@ -32,8 +34,42 @@ func Check(ctx context.Context, i IaaS) error {
 	return nil
 }
 
-func IsBalanced(s *State) bool {
+type hostList []string
+
+func (h hostList) exceedsMax(hostCount int) bool {
+	totalJobs := len(h)
+	maxJobsPerHost := int(math.Ceil(float64(totalJobs) / float64(hostCount)))
+
+	counts := make(map[string]int)
+	for _, host := range h {
+		counts[host] = counts[host] + 1
+	}
+
+	for _, v := range counts {
+		if v > maxJobsPerHost {
+			return true
+		}
+	}
 	return false
+}
+
+// IsBalanced determines whether the state of a deployment is balanced.
+// A deployment is balanced jobs are spread across as many hosts as possible.
+func IsBalanced(s *State) bool {
+	hostCount := len(s.Hosts)
+
+	jobHosts := make(map[string]hostList)
+	for _, vm := range s.VMs {
+		jobHosts[vm.Job] = append(jobHosts[vm.Job], vm.Host)
+	}
+
+	for jobName, hosts := range jobHosts {
+		if hosts.exceedsMax(hostCount) {
+			log.Printf("job %s is not balanced", jobName)
+			return false
+		}
+	}
+	return true
 }
 
 func Balance(s *State) *State {
--- state_test.go
+++ state_test.go
@@ -39,4 +39,134 @@ var _ = Describe("State", func() {
 			Ω(err).Should(Equal(noState))
 		})
 	})
+
+	Context("IsBalanced", func() {
+		It("reports a single-host state as balanced", func() {
+			state := &magnet.State{
+				Hosts: []*magnet.Host{
+					&magnet.Host{},
+				},
+			}
+			Ω(magnet.IsBalanced(state)).Should(BeTrue())
+		})
+
+		It("detects an unbalanced state when two jobs are on the same host", func() {
+			host1 := &magnet.Host{ID: "host1"}
+			host2 := &magnet.Host{ID: "host2"}
+			routerVM1 := &magnet.VM{Job: "router", Host: host1.ID}
+			routerVM2 := &magnet.VM{Job: "router", Host: host1.ID}
+
+			state := &magnet.State{
+				Hosts: []*magnet.Host{host1, host2},
+				VMs:   []*magnet.VM{routerVM1, routerVM2},
+			}
+			Ω(magnet.IsBalanced(state)).Should(BeFalse())
+		})
+
+		It("detects a balanced state when two jobs are on separate hosts", func() {
+			host1 := &magnet.Host{ID: "host1"}
+			host2 := &magnet.Host{ID: "host2"}
+			routerVM1 := &magnet.VM{Job: "router", Host: host1.ID}
+			routerVM2 := &magnet.VM{Job: "router", Host: host2.ID}
+
+			state := &magnet.State{
+				Hosts: []*magnet.Host{host1, host2},
+				VMs:   []*magnet.VM{routerVM1, routerVM2},
+			}
+			Ω(magnet.IsBalanced(state)).Should(BeTrue())
+		})
+
+		It("detects a balanced state when 4 jobs are split on separate hosts", func() {
+			host1 := &magnet.Host{ID: "host1"}
+			host2 := &magnet.Host{ID: "host2"}
+			routerVM1 := &magnet.VM{Job: "router", Host: host1.ID}
+			routerVM2 := &magnet.VM{Job: "router", Host: host2.ID}
+			cellVM1 := &magnet.VM{Job: "diego_cell", Host: host1.ID}
+			cellVM2 := &magnet.VM{Job: "diego_cell", Host: host2.ID}
+
+			// throw some extra jobs in the mix
+			clockGlobalVM := &magnet.VM{Job: "clock_global", Host: host2.ID}
+			cloudControllerVM := &magnet.VM{Job: "cloud_controller", Host: host2.ID}
+			haproxyVM := &magnet.VM{Job: "ha_proxy", Host: host1.ID}
+
+			state := &magnet.State{
+				Hosts: []*magnet.Host{host1, host2},
+				VMs:   []*magnet.VM{routerVM1, routerVM2, cellVM1, cellVM2, clockGlobalVM, cloudControllerVM, haproxyVM},
+			}
+			Ω(magnet.IsBalanced(state)).Should(BeTrue())
+		})
+
+		It("detects an unbalanced state when 4 jobs are incorrectly split on separate hosts", func() {
+			host1 := &magnet.Host{ID: "host1"}
+			host2 := &magnet.Host{ID: "host2"}
+			// both routers on the same host, and both cells on the same host
+			routerVM1 := &magnet.VM{Job: "router", Host: host1.ID}
+			routerVM2 := &magnet.VM{Job: "router", Host: host1.ID}
+			cellVM1 := &magnet.VM{Job: "diego_cell", Host: host2.ID}
+			cellVM2 := &magnet.VM{Job: "diego_cell", Host: host2.ID}
+
+			state := &magnet.State{
+				Hosts: []*magnet.Host{host1, host2},
+				VMs:   []*magnet.VM{routerVM1, routerVM2, cellVM1, cellVM2},
+			}
+			Ω(magnet.IsBalanced(state)).Should(BeFalse())
+		})
+
+		It("detects a balanced state when some duplication is required", func() {
+			host1 := &magnet.Host{ID: "host1"}
+			host2 := &magnet.Host{ID: "host2"}
+			host3 := &magnet.Host{ID: "host3"}
+
+			routerVM1 := &magnet.VM{Job: "router", Host: host1.ID}
+			routerVM2 := &magnet.VM{Job: "router", Host: host2.ID}
+			routerVM3 := &magnet.VM{Job: "router", Host: host3.ID}
+			routerVM4 := &magnet.VM{Job: "router", Host: host1.ID}
+			routerVM5 := &magnet.VM{Job: "router", Host: host2.ID}
+
+			state := &magnet.State{
+				Hosts: []*magnet.Host{host1, host2, host3},
+				VMs:   []*magnet.VM{routerVM1, routerVM2, routerVM3, routerVM4, routerVM5},
+			}
+			Ω(magnet.IsBalanced(state)).Should(BeTrue())
+		})
+
+		It("detects an unbalanced state when some duplication is required", func() {
+			host1 := &magnet.Host{ID: "host1"}
+			host2 := &magnet.Host{ID: "host2"}
+			host3 := &magnet.Host{ID: "host3"}
+
+			routerVM1 := &magnet.VM{Job: "router", Host: host1.ID}
+			routerVM2 := &magnet.VM{Job: "router", Host: host2.ID}
+
+			// put 3 routers on a single host, when the best-case
+			// scenario is a max of 2 routers per host
+			routerVM3 := &magnet.VM{Job: "router", Host: host3.ID}
+			routerVM4 := &magnet.VM{Job: "router", Host: host3.ID}
+			routerVM5 := &magnet.VM{Job: "router", Host: host3.ID}
+
+			state := &magnet.State{
+				Hosts: []*magnet.Host{host1, host2, host3},
+				VMs:   []*magnet.VM{routerVM1, routerVM2, routerVM3, routerVM4, routerVM5},
+			}
+			Ω(magnet.IsBalanced(state)).Should(BeFalse())
+		})
+
+		It("detects a balanced state when some hosts are not utilized", func() {
+			host1 := &magnet.Host{ID: "host1"}
+			host2 := &magnet.Host{ID: "host2"}
+			host3 := &magnet.Host{ID: "host3"}
+			host4 := &magnet.Host{ID: "host4"}
+			host5 := &magnet.Host{ID: "host5"}
+
+			routerVM1 := &magnet.VM{Job: "router", Host: host1.ID}
+			routerVM2 := &magnet.VM{Job: "router", Host: host2.ID}
+			routerVM3 := &magnet.VM{Job: "router", Host: host3.ID}
+
+			state := &magnet.State{
+				Hosts: []*magnet.Host{host1, host2, host3, host4, host5},
+				VMs:   []*magnet.VM{routerVM1, routerVM2, routerVM3},
+			}
+			Ω(magnet.IsBalanced(state)).Should(BeTrue())
+		})
+	})
 })
