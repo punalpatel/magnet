694eda878ace047e52964bab264c72f8dae0d6be
Zac Bergquist <zbergquist99@gmail.com>
2016-10-12 16:16:15 -0600

    Add versions and environment metadata to builds

--- ci/run-unit-tests.sh
+++ ci/run-unit-tests.sh
@@ -4,6 +4,10 @@ set -xe
 
 source_dir="$(cd "$(dirname "$0")" && pwd)"
 pushd $source_dir/..
+  echo "PWD: $(pwd)"
+  echo "GOPATH: $GOPATH"
+  go version
+  glide -v
   glide install
   go test -v -race $(glide nv)
 popd
