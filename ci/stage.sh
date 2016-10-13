220176269724395c4c0d3833b2579665b0b92782
Zac Bergquist <zbergquist99@gmail.com>
2016-10-13 10:22:44 -0600

    Fix version

--- ci/stage.sh
+++ ci/stage.sh
@@ -12,8 +12,8 @@ pushd $source_dir/..
   mkdir -p $stage_dir/release
   shorthash=$(git rev-parse --short HEAD)
   tag=$(git describe --abbrev=0 --tags)
-  GOOS=linux GOARCH=amd64 go build -ldflags '-w -s -X main.Version=$tag-$shorthash' -o $stage_dir/release/magnet-linux ./cmd/magnet
-  GOOS=darwin GOARCH=amd64 go build -ldflags '-w -s -X main.Version=$tag-$shorthash' -o $stage_dir/release/magnet-darwin ./cmd/magnet
-  GOOS=windows GOARCH=amd64 go build -ldflags '-w -s -X main.Version=$tag-$shorthash' -o $stage_dir/release/magnet.exe ./cmd/magnet
+  GOOS=linux GOARCH=amd64 go build -ldflags "-w -s -X main.Version=$tag-$shorthash" -o $stage_dir/release/magnet-linux ./cmd/magnet
+  GOOS=darwin GOARCH=amd64 go build -ldflags "-w -s -X main.Version=$tag-$shorthash" -o $stage_dir/release/magnet-darwin ./cmd/magnet
+  GOOS=windows GOARCH=amd64 go build -ldflags "-w -s -X main.Version=$tag-$shorthash" -o $stage_dir/release/magnet.exe ./cmd/magnet
   echo $tag > $stage_dir/tag
 popd
